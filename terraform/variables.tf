0a0e8257a0ebed734ffb14a7a267bd53d63bc9ef
Francisco Ndong <93534625+fndongn@users.noreply.github.com>
2026-02-18 11:30:47 -0600

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,6 +2,14 @@ variable "region" {
   default = "us-east-1"
 }
 
+variable "az_a" {
+  default = "us-east-1a"
+}
+
+variable "az_b" {
+  default = "us-east-1b"
+}
+
 variable "project_name" {
   default = "eks-deployment"
 }
@@ -12,6 +20,10 @@ variable "environment" {
 
 variable "vpc_cidr" {
   default = "10.0.0.0/16"
+}
+
+variable "subnet_cidr" {
+  default = "10.0.0.0/24"
 }
 
 variable "key_name" {
